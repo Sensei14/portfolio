f42c8da773c47e22af4b17d5c47c34316dbd0995
unknown <karox1241@wp.pl>
2020-01-08 19:28:02 +0100

    Updates

---
+++ precache-manifest.1159ff56f9c01c649aebbf5bc45a6e51.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0744444274f5fa73f2b1264c2f505c33",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d0d6aa434fd12eba7b22",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3a87729f95c3488154a0",
+    "url": "/portfolio/static/css/main.0e88d5aa.chunk.css"
+  },
+  {
+    "revision": "d0d6aa434fd12eba7b22",
+    "url": "/portfolio/static/js/2.1babe821.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.1babe821.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a87729f95c3488154a0",
+    "url": "/portfolio/static/js/main.54e7bbf1.chunk.js"
+  },
+  {
+    "revision": "e139b2548d94e481a5d0",
+    "url": "/portfolio/static/js/runtime-main.0762ecda.js"
+  },
+  {
+    "revision": "0ada786de142cfca64cf123e64937a52",
+    "url": "/portfolio/static/media/QuEasy1.0ada786d.jpg"
+  },
+  {
+    "revision": "876f06e0ba8e4dff1628f1fb7fde1402",
+    "url": "/portfolio/static/media/QuEasy2.876f06e0.jpg"
+  },
+  {
+    "revision": "44119ee607295f6cf8053da198fbe30d",
+    "url": "/portfolio/static/media/SpaceInvaders.44119ee6.png"
+  },
+  {
+    "revision": "d4c5093690f97a450c0d46c691f07ec2",
+    "url": "/portfolio/static/media/ai1.d4c50936.png"
+  },
+  {
+    "revision": "f4fa2e891d17cf8f6bfb4f2167de1760",
+    "url": "/portfolio/static/media/ai2.f4fa2e89.png"
+  },
+  {
+    "revision": "c0a31509764615b1ce4b758c5eb97b19",
+    "url": "/portfolio/static/media/ai3.c0a31509.png"
+  },
+  {
+    "revision": "4989fdfc81b92dbb47fa1c0a9744d4d5",
+    "url": "/portfolio/static/media/ai4.4989fdfc.png"
+  },
+  {
+    "revision": "e5c7c5fd20483803db45bc90b65a5cf9",
+    "url": "/portfolio/static/media/ai5.e5c7c5fd.png"
+  },
+  {
+    "revision": "da53dc3418ca99ce8fec3846274d9cb8",
+    "url": "/portfolio/static/media/bootstrap.da53dc34.png"
+  },
+  {
+    "revision": "af9977213a1d348f6f2ac1575bcb88c6",
+    "url": "/portfolio/static/media/game1.af997721.png"
+  },
+  {
+    "revision": "4d3271d5110548b2b7213e2f693f3a01",
+    "url": "/portfolio/static/media/game2.4d3271d5.png"
+  },
+  {
+    "revision": "251ca88bc8a815bdf27c8d68aa615f44",
+    "url": "/portfolio/static/media/game3.251ca88b.png"
+  },
+  {
+    "revision": "57dc5d4e609c17e4dff6cb0a7b715e97",
+    "url": "/portfolio/static/media/html.57dc5d4e.png"
+  },
+  {
+    "revision": "19772f5ae7273556d9b169674447b22a",
+    "url": "/portfolio/static/media/itunes1.19772f5a.png"
+  },
+  {
+    "revision": "538ddb8dd8adc6b1527b418ff7acc60c",
+    "url": "/portfolio/static/media/itunes2.538ddb8d.png"
+  },
+  {
+    "revision": "277d252279241230ddfb8c55b27ee20c",
+    "url": "/portfolio/static/media/mongodb.277d2522.png"
+  },
+  {
+    "revision": "0e4aad1cfe97d21c1bcd68e882f197f7",
+    "url": "/portfolio/static/media/nodejs.0e4aad1c.png"
+  },
+  {
+    "revision": "80687d23d8eef70e255e1996a06d9f5c",
+    "url": "/portfolio/static/media/react.80687d23.png"
+  },
+  {
+    "revision": "8539cc4970d5b2b5a89ca0659196d506",
+    "url": "/portfolio/static/media/todo.8539cc49.png"
+  },
+  {
+    "revision": "0226ff06673fda34217579dd78120a1c",
+    "url": "/portfolio/static/media/weather1.0226ff06.png"
+  },
+  {
+    "revision": "a5e1946185d8d7e99e37d3693ccc0c17",
+    "url": "/portfolio/static/media/weather2.a5e19461.png"
+  },
+  {
+    "revision": "c8a3e59b298708173fe8daf831d486b2",
+    "url": "/portfolio/static/media/zdj.c8a3e59b.png"
+  }
+]);
